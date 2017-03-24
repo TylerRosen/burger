b2b1b8dfb04f06b328de3100662cc86f3808b9c1
Tyler Rosen <tylermrosen@gmail.com>
2017-03-24 14:38:15 -0400

    Made connection file

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-// Packages
+// Dependencies
 
 var express = require('express');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override')
-
-var port = 3000;
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
 
 var app = express();
+var port = 3000;
 
-// Displays static content
+// Displays static content from public folder
 app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Handlebars
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(port);
